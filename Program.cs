bf3ae24a531358a18d4deec63da0893da4fe9a0a
HyperData3434 <76264118+HyperData3434@users.noreply.github.com>
2021-10-28 09:37:40 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("Console calculator\r");
+            Console.WriteLine("------------------\n");
+
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("\t+ - Add");
+            Console.WriteLine("\t- - Subtract");
+            Console.WriteLine("\t* - Multiply");
+            Console.WriteLine("\t/ - Divide");
+            Console.Write("Your option- ");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{num1} + {num2} = " + (num1 + num2));
+                    Console.ResetColor();
+                    break;
+                case "-":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{num1} - {num2} = " + (num1 - num2));
+                    Console.ResetColor();
+                    break;
+                case "*":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{num1} * {num2} = " + (num1 * num2));
+                    Console.ResetColor();
+                    break;
+                case "/":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"{num1} / {num2} = " + (num1 / num2));
+                    Console.ResetColor();
+                    break;
+            }
+            Console.WriteLine("Press return/enter to close the Calculator console app.");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) {}
+        }
+    }
+}
