667b9b620e04af7dc79a771d2ee6b0cb53f7d425
HyperData3434 <76264118+HyperData3434@users.noreply.github.com>
2022-01-05 09:34:21 -0600

    MoRe CoLorS and Small Changes to Interface

--- Program.cs
+++ Program.cs
@@ -9,21 +9,29 @@ namespace Calc
             int num1 = 0;
             int num2 = 0;
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Console calculator\r");
             Console.WriteLine("------------------\n");
+            Console.ResetColor();
             
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Choose an option");
-            Console.WriteLine("\t+ - Add");
-            Console.WriteLine("\t- - Subtract");
-            Console.WriteLine("\t* - Multiply");
-            Console.WriteLine("\t/ - Divide");
+            Console.WriteLine("\t+ = Add");
+            Console.WriteLine("\t- = Subtract");
+            Console.WriteLine("\t* = Multiply");
+            Console.WriteLine("\t/ = Divide");
+            Console.ResetColor();
             Console.WriteLine("Type your option then pres Enter");
             var Operation = Console.ReadLine();
             
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Type your first number, and then press Enter");
+            Console.ResetColor();
             num1 = Convert.ToInt32(Console.ReadLine());
 
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Type another number, and then press Enter");
+            Console.ResetColor();
             num2 = Convert.ToInt32(Console.ReadLine());
             
             switch (Operation)
@@ -57,6 +65,7 @@ namespace Calc
                     Console.ResetColor();
                     break;
             }
+            Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Press return/enter to close the Calculator console app.");
             Console.ResetColor();
